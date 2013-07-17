2ecd8b21ea0173c0e8faf0431561ef9790f46a69
anil.bharadia <anil.bharadia@gmail.com>
2013-07-17 19:50:58 +0530

    added some common methods

--- RW1/src/org/rw/dao/GenericDao.java
+++ RW1/src/org/rw/dao/GenericDao.java
@@ -5,18 +5,34 @@ import java.util.List;
 
 public interface GenericDao <E, PK extends Serializable> {
 
+	
     /** Persist the newInstance object into database */
     PK create(E newInstance);
 
+    
     /** Retrieve an object that was previously persisted to the database using the indicated id as primary key */
     E read(PK id);
 
+    
     /** Save changes made to a persistent object.  */
     void update(E transientObject);
 
+    
     /** Remove an object from persistent storage in the database */
     void delete(E persistentObject);
     
+    
     List<E> findByProperty(String propertyName, String propertyValue);
+    
+    
+    List<E> findByExample(E entity);
+
+    
+    List<E> findByLikeExample(E entity);
+    
+    
+    List<E> findAll();
+    
+    
 }
 
